0aecdddd989e5538cb403fb853c1951172635e6b
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-26 23:42:24 +0900

    create edit destroy

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,10 +25,35 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 
 end
