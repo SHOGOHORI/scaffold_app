20889478a487ce80f7d983a2064c7bcfcabbf2b5
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-28 20:41:57 +0900

    add user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,53 @@
 class UsersController < ApplicationController
 
-  def new
+  def index
+    @users = User.all
   end
 
-  def create    
+
+  def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        redirect_to @user 
+        flash[:notice] = 'User was successfully created.' 
+      else
+        render :new 
+      end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(user_params)
+      if @user.save
+        redirect_to @user 
+        flash[:notice] = 'User was successfully updated.' 
+      else
+        render :new 
+      end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url
+    flash[:notice] = 'Blog was successfully destroyed.' 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe User, type: :model do
                      email:"shogo@example.com")
     expect(user).to be_valid
   end
+
+  it "空欄を投稿する" do
+    user = User.new( name: "",
+                     email: "")
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
 end
