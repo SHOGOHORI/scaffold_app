c921d801114e0f6ade843e54d2b117bfeecd258d
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-28 16:02:08 +0900

    Add rspec test

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -5,9 +5,32 @@ RSpec.describe Blog, type: :model do
   it "2と1を足すと3になること" do
     expect(2 + 1).to eq 3
   end
+
   it "投稿に成功する" do
     blog = Blog.new( title: "aaaa",
                      content:"bbbb")
     expect(blog).to be_valid
+  end
+
+  it "空欄を投稿する" do
+    blog = Blog.new( title: "",
+                     content:"")
+    blog.valid?
+    expect(blog.errors[:title]).to include("can't be blank")
+    expect(blog.errors[:content]).to include("can't be blank")
+  end
+
+  it "長過ぎるタイトルを投稿する" do
+    blog = Blog.new( title: "a" * 51,
+                     content:"aaaaa")
+    blog.valid?
+    expect(blog.errors[:title]).to include("is too long (maximum is 50 characters)")
+  end
+
+  it "長過ぎるコンテンツを投稿する" do
+    blog = Blog.new( title: "aaaaa",
+                     content:"a" * 257)
+    blog.valid?
+    expect(blog.errors[:content]).to include("is too long (maximum is 255 characters)")
   end
 end
