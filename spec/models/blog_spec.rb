1da4c66c3c31c39989f5db402f1a962cdfce45fa
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-29 12:20:06 +0900

    add sample_rspec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,36 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  # 姓、名、メール、パスワードがあれば有効な状態であること
-  it "2と1を足すと3になること" do
-    expect(2 + 1).to eq 3
-  end
+  describe "投稿" do
+    it "投稿に成功する" do
+      blog = Blog.new( title: "aaaa",
+                       content:"bbbb")
+      expect(blog).to be_valid
+    end
 
-  it "投稿に成功する" do
-    blog = Blog.new( title: "aaaa",
-                     content:"bbbb")
-    expect(blog).to be_valid
-  end
+    it "タイトルとコンテンツが空欄にすることを許可しない" do
+      blog = Blog.new( title: "",
+                       content:"")
+      blog.valid?
+      expect(blog.errors[:title]).to include("can't be blank")
+      expect(blog.errors[:content]).to include("can't be blank")
+    end
 
-  it "タイトルとコンテンツが空欄にすることを許可しない" do
-    blog = Blog.new( title: "",
-                     content:"")
-    blog.valid?
-    expect(blog.errors[:title]).to include("can't be blank")
-    expect(blog.errors[:content]).to include("can't be blank")
-  end
+    it "タイトルが長すぎることを許可しない" do
+      blog = Blog.new( title: "a" * 51,
+                       content:"aaaaa")
+      blog.valid?
+      expect(blog.errors[:title]).to include("is too long (maximum is 50 characters)")
+    end
 
-  it "タイトルが長すぎることを許可しない" do
-    blog = Blog.new( title: "a" * 51,
-                     content:"aaaaa")
-    blog.valid?
-    expect(blog.errors[:title]).to include("is too long (maximum is 50 characters)")
-  end
-
-  it "コンテンツが長すぎることを許可しない" do
-    blog = Blog.new( title: "aaaaa",
-                     content:"a" * 257)
-    blog.valid?
-    expect(blog.errors[:content]).to include("is too long (maximum is 255 characters)")
+    it "コンテンツが長すぎることを許可しない" do
+      blog = Blog.new( title: "aaaaa",
+                       content:"a" * 257)
+      blog.valid?
+      expect(blog.errors[:content]).to include("is too long (maximum is 255 characters)")
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,51 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "ユーザー認証" do
+    it "ユーザーの作成に成功する" do
+      user = User.new( name: "shogo",
+                       email:"shogo@example.com")
+      expect(user).to be_valid
+    end
 
-  it "ユーザーの作成に成功する" do
-    user = User.new( name: "shogo",
-                     email:"shogo@example.com")
-    expect(user).to be_valid
+    it "名前とメールアドレスを空欄にすることを許可しない" do
+      user = User.new( name: "",
+                       email: "")
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "名前が長すぎることを許可しない" do
+      user = User.new( name: "a" * 51,
+                       email:"aaaaa")
+      user.valid?
+      expect(user.errors[:name]).to include("is too long (maximum is 50 characters)")
+    end
+
+    it "メールアドレスが長すぎることを許可しない" do
+      user = User.new( name: "aaa",
+                       email:"aaaaa" * 257)
+      user.valid?
+      expect(user.errors[:email]).to include("is too long (maximum is 255 characters)")
+    end
+
+    it "メールアドレスが重複していることを許可しない" do
+      User.create( name: "shogo",
+                   email:"shogo@example.com")
+      user = User.new( name: "yamada",
+                    email:"shogo@example.com")
+      user.valid?
+      expect(user.errors[:email]).to include("has already been taken")
+    end
+
+    it "2人のユーザーが同じ名前を使うことを許可する" do
+      User.create( name: "shogo",
+                   email:"shogo@example.com")
+      user = User.new( name: "shogo",
+                    email:"yamada@example.com")
+      user.valid?
+      expect(user).to be_valid
+    end
   end
-
-  it "名前とメールアドレスを空欄にすることを許可しない" do
-    user = User.new( name: "",
-                     email: "")
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-
-  it "名前が長すぎることを許可しない" do
-    user = User.new( name: "a" * 51,
-                     email:"aaaaa")
-    user.valid?
-    expect(user.errors[:name]).to include("is too long (maximum is 50 characters)")
-  end
-
-  it "メールアドレスが長すぎることを許可しない" do
-    user = User.new( name: "aaa",
-                     email:"aaaaa" * 257)
-    user.valid?
-    expect(user.errors[:email]).to include("is too long (maximum is 255 characters)")
-  end
-
-  it "メールアドレスが重複していることを許可しない" do
-    User.create( name: "shogo",
-                 email:"shogo@example.com")
-    user = User.new( name: "yamada",
-                  email:"shogo@example.com")
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
-  end
-
-  it "2人のユーザーが同じ名前を使うことを許可する" do
-    User.create( name: "shogo",
-                 email:"shogo@example.com")
-    user = User.new( name: "shogo",
-                  email:"yamada@example.com")
-    user.valid?
-    expect(user).to be_valid
-  end
-
 end
