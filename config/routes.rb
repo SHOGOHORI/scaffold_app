1dd987b6c0cb541c922854d9d8f34d4e9bbe23ad
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-10-05 00:45:02 +0900

    change routes2

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :blogs
-  resources :users
 end
